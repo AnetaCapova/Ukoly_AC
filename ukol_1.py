191862a7f226d0eb3a6ea74fa0a4faa47292ff9d
Aneta Capova <anetsa.capova@seznam.cz>
2022-09-24 19:48:59 +0200

    Zkouším přidat první úkol

---
+++ ukol_1.py
@@ -0,0 +1,15 @@
+baliky = {
+    "B541X": True,
+    "B547X": False,
+    "B251X": False,
+    "B501X": True,
+    "B947X": False,
+}
+
+kod_baliku = input("Zadej prosím kód balíku: ")
+
+if kod_baliku in baliky:
+    baliky.values == True
+    print("Balík byl předán kurýrovi")
+else:
+    print("Balík zatím nebyl předán kurýrovi")
