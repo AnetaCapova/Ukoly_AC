3bb7f62612297aa98fab9b3776480b0d806d091c
Aneta Capova <anetsa.capova@seznam.cz>
2022-10-17 21:40:54 +0200

    hotovy ukol c.3

--- ukol_3.py
+++ ukol_3.py
@@ -1,33 +1,38 @@
-from doctest import FAIL_FAST
-from math import ceil
-
-
-cislo = input("Zadejte prosím telefonní číslo bez mezer(!): ")
-# zjistit správný formát
-pocet_cisel = len(cislo)
-
-def spravny_format(pocet_cisel):
-    if pocet_cisel == 9 or 13:
+def zkontroluj_delku_cisla(delka_cisla):
+    if delka_cisla == 9 or delka_cisla == 13:
         return True
     else:
         return False
 
-text_zpravy = input("Vypište prosím text zprávy: ")
-# kolik bude zpráva stát
 
 
-def cena_zpravy():
+zadane_cislo = input("Zadejte prosím telefonní číslo bez mezer(!): ")
+# zjistit správný formát
+pocet_znaku_cisla = len(zadane_cislo)
+
+zkontroluj_delku_cisla(pocet_znaku_cisla)
+# print(pocet_znaku_cisla)
+
+if zkontroluj_delku_cisla(pocet_znaku_cisla) == False:
+    exit()
+
+import math
+from math import ceil
+
+def vypocti_cenu_zpravy(text_zpravy):
     delka_zpravy = len(text_zpravy)
     cena = 3
     jedna_zprava = 180
-    # mena = "kč"
     if delka_zpravy <= jedna_zprava:
         cena_zpravy = cena
     elif delka_zpravy > jedna_zprava:
-        import math
-        skutecna_delka = delka_zpravy / jedna_zprava
-        cena_zpravy = math.ceil(skutecna_delka) * cena
+        pocet_zprav = delka_zpravy / jedna_zprava
+        cena_zpravy = math.ceil(pocet_zprav) * cena
+    return cena_zpravy
 
-# print(spravny_format())
-# print(cena_zpravy())
-print(cena_zpravy)
+zadany_text_zpravy = input("Vypište prosím text zprávy: ")
+# kolik bude zpráva stát
+
+konecna_cena = vypocti_cenu_zpravy(zadany_text_zpravy)
+
+print(konecna_cena)
